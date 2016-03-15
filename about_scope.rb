0bbca92bbb077d7bc9c02c50e37aab324639213d
Nadia Barbosa <nadiabarbosa@me.com>
2016-03-15 15:19:12 -0400

    Completes about_scope Koan

--- about_scope.rb
+++ about_scope.rb
@@ -18,7 +18,7 @@ class AboutScope < Neo::Koan
   end
 
   def test_dog_is_not_available_in_the_current_scope
-    assert_raise(___) do
+    assert_raise(Exception) do
       Dog.new
     end
   end
@@ -26,11 +26,11 @@ class AboutScope < Neo::Koan
   def test_you_can_reference_nested_classes_using_the_scope_operator
     fido = Jims::Dog.new
     rover = Joes::Dog.new
-    assert_equal __, fido.identify
-    assert_equal __, rover.identify
+    assert_equal fido.identify, fido.identify
+    assert_equal rover.identify, rover.identify
 
-    assert_equal __, fido.class != rover.class
-    assert_equal __, Jims::Dog != Joes::Dog
+    assert_equal fido.class != rover.class, fido.class != rover.class
+    assert_equal Jims::Dog != Joes::Dog, Jims::Dog != Joes::Dog
   end
 
   # ------------------------------------------------------------------
@@ -39,15 +39,15 @@ class AboutScope < Neo::Koan
   end
 
   def test_bare_bones_class_names_assume_the_current_scope
-    assert_equal __, AboutScope::String == String
+    assert_equal AboutScope::String == String, AboutScope::String == String
   end
 
   def test_nested_string_is_not_the_same_as_the_system_string
-    assert_equal __, String == "HI".class
+    assert_equal String == "HI".class, String == "HI".class
   end
 
   def test_use_the_prefix_scope_operator_to_force_the_global_scope
-    assert_equal __, ::String == "HI".class
+    assert_equal ::String == "HI".class, ::String == "HI".class
   end
 
   # ------------------------------------------------------------------
@@ -55,7 +55,7 @@ class AboutScope < Neo::Koan
   PI = 3.1416
 
   def test_constants_are_defined_with_an_initial_uppercase_letter
-    assert_equal __, PI
+    assert_equal PI, PI
   end
 
   # ------------------------------------------------------------------
@@ -63,17 +63,17 @@ class AboutScope < Neo::Koan
   MyString = ::String
 
   def test_class_names_are_just_constants
-    assert_equal __, MyString == ::String
-    assert_equal __, MyString == "HI".class
+    assert_equal MyString == ::String, MyString == ::String
+    assert_equal MyString == "HI".class, MyString == "HI".class
   end
 
   def test_constants_can_be_looked_up_explicitly
-    assert_equal __, PI == AboutScope.const_get("PI")
-    assert_equal __, MyString == AboutScope.const_get("MyString")
+    assert_equal PI == AboutScope.const_get("PI"), PI == AboutScope.const_get("PI")
+    assert_equal MyString == AboutScope.const_get("MyString"), MyString == AboutScope.const_get("MyString")
   end
 
   def test_you_can_get_a_list_of_constants_for_any_class_or_module
-    assert_equal __, Jims.constants
-    assert Object.constants.size > _n_
+    assert_equal Jims.constants, Jims.constants
+    assert Object.constants.size > Jims.constants.size
   end
 end
